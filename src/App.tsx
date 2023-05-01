a66228ce5bc0ae677ee8eaf735440a12383651c2
qwerewqwerew <qwerew0@gmail.com>
2023-05-01 22:58:06 +0900

    05-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import Todos from "./components/Todos";
+import MyTodo from "./components/MyTodo";
 import Todo from "./models/todo";
-
 function App() {
-	const todos=[
-		new Todo("리액트공부","함"),
-		new Todo("타입스크립트 공부","안함"),
+	const todos = [
+		new Todo("리액트공부", "함"),
+		new Todo("타입스크립트 공부", "안함"),
 	];
 	return (
 		<div className="App">
+			<MyTodo />
 			<Todos items={todos} />
 		</div>
 	);
--- src/components/MyTodo.tsx
+++ src/components/MyTodo.tsx
@@ -1,11 +1,21 @@
+import { useRef } from "react";
 const MyTodo = () => {
-	const submitHandler = (e:React.FormEvent) => {
-		 e.preventDefault();
-	  }
+	const todoTextInput = useRef<HTMLInputElement>(null);
+	const todoNumInput = useRef<HTMLInputElement>(null);
+	const submitHandler = (e: React.FormEvent) => {
+		e.preventDefault();
+		const enteredText = todoTextInput.current!.value;
+		if (enteredText.trim().length === 0) {
+			return;
+		}
+
+		const enteredNum: number = todoTextInput.current!.valueAsNumber;
+	}
 	return (
 		<form onSubmit={submitHandler}>
 			<label htmlFor="text">할일내용:</label>
-			<input type="text" id="text" />
+			<input type="text" id="text" ref={todoTextInput} />
+			<input type="number" ref={todoNumInput} />
 			<button>추가</button>
 		</form>
 	);
